f09ab23908898388bdefc3d5876ee36a94e1c6fc
saci zakaria <sacideveloppeur@gmail.com>
2021-08-20 22:14:00 +0200

    Init Widgets statFull M.Passeel

---
+++ lib/Interactive_Page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class InteractifPage extends StatefulWidget {
+  @override
+  // State<StatefulWidget> createState() {
+  //   return InteractifPageStat();
+
+  InteractifPageState createState() => InteractifPageState();
+  }
+
+
+class InteractifPageState extends State<InteractifPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Les Interactif'),),
+      body: Center(child: Text("Apprendre Les Interactives"),),
+    );
+  }
+}
